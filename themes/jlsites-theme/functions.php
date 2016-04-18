b5bdbc74a841b261ee592fb9e2e6319ca1796c54
leorotondano <leorotondano@gmail.com>
2016-04-18 20:31:16 -0300

    Adicionando alterações para a secao sobre como fazer um site

--- themes/jlsites-theme/functions.php
+++ themes/jlsites-theme/functions.php
@@ -1,6 +1,7 @@
 <?php
 	function jlsites_enqueue_styles() {
 		wp_enqueue_style( 'jlsites-style', get_template_directory_uri() . '/style.css', array('zerif_bootstrap_style') );
+		wp_enqueue_style( 'font-awesome-style', get_stylesheet_directory_uri() . '/fonts/font-awesome-4.6.1/css/font-awesome.min.css' );
 	}
 
 	add_action( 'wp_enqueue_scripts', 'jlsites_enqueue_styles' );
