8ece71ed6ea4d89ec5d486b6f69f2024311c72fc
leorotondano <leorotondano@gmail.com>
2016-05-10 21:07:48 -0300

    Adicionando alterações para a ordenação das seções. Incluindo as seções our_services e how_we_work.

---
+++ themes/jlsites-theme/sections/how_we_work.php
@@ -0,0 +1,18 @@
+<section class="howWeWork" id="howWeWork">
+
+<div class="container">
+
+	<!-- SECTION HEADER -->
+
+	<div class="section-header">
+		how we work
+	
+	</div>
+
+	<div class="row">
+
+	</div>
+
+</div> <!-- / END CONTAINER -->
+
+</section>  <!-- / END FOCUS SECTION -->	
---
+++ themes/jlsites-theme/sections/our_service.php
@@ -0,0 +1,18 @@
+<section class="service" id="service">
+
+<div class="container">
+
+	<!-- SECTION HEADER -->
+
+	<div class="section-header">
+		our services
+	
+	</div>
+
+	<div class="row">
+
+	</div>
+
+</div> <!-- / END CONTAINER -->
+
+</section>  <!-- / END FOCUS SECTION -->	
