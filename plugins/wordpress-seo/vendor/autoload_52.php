9d0cafe698057865dcf277e452901eae161a125a
João Paulo Paschoal <jpvitor.neto@gmail.com>
2017-01-31 21:35:07 -0300

    Parametrizado a seção de publicações

--- plugins/wordpress-seo/vendor/autoload_52.php
+++ plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit0324dd2ea047548da5b10016d34cd19e::getLoader();
+return ComposerAutoloaderInitfa6420bc85224ec353200fcb336e5d14::getLoader();
